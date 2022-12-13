994a09f3f2f26a120d7a2aa6544126865428b31b
Ikbal Aslan <ikball.aslann@gmail.com>
2022-12-13 18:46:00 +0300

    Upd-7

---
+++ base_urls/RegresBaseUrl.java
@@ -0,0 +1,14 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class RegresBaseUrl {  protected RequestSpecification spec;
+
+
+    @Before//If you use @Before annotation at the top of a method, it will be executed before every test method.
+    public void setUp(){
+        spec = new RequestSpecBuilder().setBaseUri("https://reqres.in/api").build();
+    }
+}
