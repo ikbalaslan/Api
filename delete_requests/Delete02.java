2b42f35298a073ae44a029a4c2ab0ecd45d49e88
Ikbal Aslan <ikball.aslann@gmail.com>
2022-12-13 18:46:36 +0300

    Upd-9

---
+++ delete_requests/Delete02.java
@@ -0,0 +1,39 @@
+package delete_requests;
+
+import base_urls.HerOkuAppBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import utils.AuthenticationHerOkuApp;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static utils.AuthenticationHerOkuApp.generateToken;
+
+public class Delete02 extends HerOkuAppBaseUrl {
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking/{bookingId}
+        When
+	 		I send DELETE Request to the Url
+	 	Then
+		 	Status code is 200
+		 	And Response body is "Created"
+     */
+@Test
+    public void delete02(){
+//Set the URL
+    spec.pathParams("first","booking","second",522);
+    //Set the expected data
+    String expectedData = "Created";
+    //Send the request get the response
+    Response res = given().spec(spec).
+            headers("Cookie", "token="+ generateToken()).
+            contentType(ContentType.JSON).
+            when().
+            delete("/{first}/{second}");
+       res.prettyPrint();
+       //Do assertion
+    assertEquals(expectedData,res.asString());
+}
+}
